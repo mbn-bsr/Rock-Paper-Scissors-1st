183af3d7c2820b446e71a77e4ed1b483f999d456
Mubeen Basheer <mubeen.basheer@gmail.com>
2024-11-28 12:21:38 +0000

    saving this code because its not as it should function, conna try again

--- main.js
+++ main.js
@@ -1,65 +1,94 @@
-//watch https://www.youtube.com/watch?v=li7FzDHYZpc
-// https://www.w3schools.com/js/js_promise.asp
-// https://www.w3schools.com/js/js_callback.asp
 
+let userName;
+let screen = document.querySelectorAll("p");
+const modalBox = document.getElementById("modal");
+const closeBtn = document.getElementById("close");
+var nameInput = document.getElementById("input");
+const questions = document.getElementById("name-move");
+const okBtn = document.getElementById("ok-btn");
 
-const validNameCheck = () => {
-    userName = prompt('Please enter your first name');
-    if (userName.length > 15 || (/[0-9.*+?^=!:${}()|\[\]\/\\]+/g).test(userName)){
-        alert('Enter a valid name please!');
-        validNameCheck();
-    } else{
-        alert(`Welcome ${userName}, lets play!`);
-    }
-} 
-validNameCheck();
+// const popUp = () => {
+//   modalBox.style.display = "block";
+// };
 
-let screen = document.querySelectorAll('p');
-let gameCounter = 1;
-const playAgain = ()=> {
-    if(confirm('Again?'))
-    {
-         playGame();
-        screen[0].innerText =`Attempt number ${++gameCounter}.`;
-    }   
-}
+// const close = () => {
+//   modalBox.style.display = "none";
+// };
+// //---------The X button 
+// closeBtn.addEventListener("click", close);
 
-let userMove, computerMove;
-let userWon = 0; 
+//--------Function to clear the fields upon page reload
+window.onload = function () {
+  //nameInput.value = '';
+  document.querySelector("form").reset();
+  questions.innerHTML = `Please enter your first name!`;
+};
+
+okBtn.addEventListener("click", () => {
+    console.log('do i run too?');
+  if (
+    nameInput.value.length > 15 ||
+    /[0-9.*+?><,#^=!:${}()|\[\]\/\\]+/g.test(nameInput.value)
+  ) 
+  {
+    questions.innerHTML = `No numbers or special characters and no more than 15 characters allowed`;
+  } else {
+    questions.innerHTML = `Welcome ${nameInput.value}! Let's play Rock Paper and Scissors!`;
+    console.log('do i run again and again~~~~~~😊😜?????');
+    setTimeout(playGame, 5500);
+  }
+});
+
+// let gameCounter = 1;
+// const playAgain = () => {
+//   if (confirm("Again?")) {
+//     playGame();
+//     screen[0].innerText = `Attempt number ${++gameCounter}.`;
+//   }
+// };
+
+let computerMove;
+let userWon = 0;
 let computerWon = 0;
-const userWins = () => {
-    return (
-        (userMove === 'rock' && computerMove === 'scissors') ||
-        (userMove === 'paper' && computerMove === 'rock') ||
-        (userMove === 'scissors' && computerMove === 'paper')
-    );
-}
 
 //TRY TO ANNOUNCE WHO THE WINNER IS
+
+//-----GAME PLAN
 const playGame = () => {
-    userMove = prompt('enter your move', 'eg rock, paper or scissors').toLowerCase();
-    let moves = ['rock', 'paper', 'scissors'];
-    let validMove = moves.includes(userMove);
-    computerMove = moves[Math.floor(Math.random()*3)];
-    if (validMove){
+  document.querySelector('form').reset(); //CLEAR INPUT FIELD AGAIN
+  questions.innerHTML = `OK, Enter your move, eg rock, paper or scissors`;
+  okBtn.addEventListener('click', ()=>{
+      let userMove = nameInput.value;
+      console.log("im running");
+      console.log(userMove);
+
+      let moves = ["rock", "paper", "scissors"];
+      let validMove = moves.includes(userMove);
+      computerMove = moves[Math.floor(Math.random() * 3)];
+      if (validMove) {
+
+        let userWins = () => { 
+            console.log(nameInput.value);
+            let userMove = nameInput.value;          
+          return (
+            (userMove === "rock" && computerMove === "scissors") ||
+            (userMove === "paper" && computerMove === "rock") ||
+            (userMove === "scissors" && computerMove === "paper")
+          );
+        };
+
+        console.log(`user winning scenaria output is ${userWins()}`);
         screen[1].textContent = `Your ${userMove} vs my ${computerMove}`;
-        if(userWins()){
-            screen[2].textContent = 'You Win!';
-            screen[3].textContent = `Your score: ${++userWon}`;
-        } else if (userMove === computerMove) {
-            screen[2].textContent = `It is a Draw! Try again!`
+
+        if (userWins()) {questions.innerHTML = `You win`;
+          screen[3].textContent = `Your score: ${++userWon}`;
+        } else if (userMove === computerMove) {questions.innerHTML = `It is a Draw! Try again!`;
+        } else {questions.innerHTML = `Sorry, You lost, Try again`;
+          screen[4].textContent = `Computer score: ${++computerWon}`;
         }
-        else{
-            screen[2].textContent = `Sorry ${userName}, You lost, Try again`;
-            screen[4].textContent = `Computer score: ${++computerWon}`;
-        }        
-    }
-    else {
-        screen[1].textContent = `Please enter a valid move`;
-    }
-    setTimeout(playAgain,2000);
-}; playGame();
+      } else {questions.innerHTML = `Make sure to spell correctly`;
+      }
+  });//why is it running again? It is running again because it doesn't stop to wait for input after line 59 and finishes running. But when entered false input then it executes both event lisetners.
+};
 
 
-
-    
